c885eb96d78bba82517964fe9df7f779bde2fcd7
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-06-20 14:14:48 +0700

    Updating deliver.sh for prompt

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,9 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
-
-echo 'The following command prompts the deploy stage.'
-set -x
-# Add your deploy command here
-set +x
+java -jar target/${NAME}-${VERSION}.jar & echo $! > .pidfile
