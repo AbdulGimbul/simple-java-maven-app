4370b457d360cad6bd422033a53532b7e4a7d943
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-08-03 17:11:05 +0700

    Update jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if pgrep -f my-app-1.0-SNAPSHOT.jar.jar; then
-    pkill -f my-app-1.0-SNAPSHOT.jar.jar
+if pgrep -f my-app-1.0-SNAPSHOT.jar; then
+    pkill -f my-app-1.0-SNAPSHOT.jar
 fi
 cd app
-java -jar my-app-1.0-SNAPSHOT.jar.jar &
+java -jar my-app-1.0-SNAPSHOT.jar &
